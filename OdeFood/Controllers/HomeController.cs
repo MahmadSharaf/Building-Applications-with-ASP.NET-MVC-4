9686bb6e772498892d5761f84fd1f61760511917
Mohamed Sharaf <mahmad.sharaf@gmail.com>
2018-11-22 18:15:09 +0200

    Create unit Test Rename to OdeToFood

--- OdeFood/Controllers/HomeController.cs
+++ OdeFood/Controllers/HomeController.cs
@@ -23,9 +23,9 @@ namespace OdeFood.Controllers
 
             //? Second approach uses Model
             var model = new AboutModel();
-            model.Name = "Sharaf";
-            model.Location = "Cairo, Egypt";
-            return View(model); //sends the Model "model" to the view
+            model . Name     = "Sharaf"       ; 
+            model . Location = "Cairo, Egypt" ; 
+            return View(model); //sends the Model object "model" to the view
         }
 
         public ActionResult Contact()
@@ -37,7 +37,7 @@ namespace OdeFood.Controllers
     }
 }
 
-//? There are a couple of ways to get infomation to the view. 
+//? There are a couple of ways to get information to the view. 
 //* One approach is to put information in "ViewBag.Message()". "ViewBag" is a dynamically typed object in C Sharp.
 // That means any sort of property can be put to ViewBag and it will be available inside of the view to pull out and retrieve and display.
 //* Another approach is using models 
